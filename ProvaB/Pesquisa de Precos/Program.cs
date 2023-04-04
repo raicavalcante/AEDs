25c61b5e6cfcb41929a34588c08fd22557032925
Raí Átila <raiacavalcante@gmail.com>
2023-04-04 16:39:17 -0300

    Cria ProvaPratica1

---
+++ ProvaB/Pesquisa de Precos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args){
+        int E,cont=0;
+        E = int.Parse(Console.ReadLine());
+        double A, G;
+        for(int i = 0; i < E; i++){
+            string entrada = (Console.ReadLine());
+            string[] array = entrada.Split(' ');
+            A = double.Parse(array[1]);
+            G = double.Parse(array[2]);
+            double temp = G * 0.7;
+            if(A <= temp){
+                Console.WriteLine(array[0]);
+            }
+            else{
+                cont++;
+            }
+        }
+        if(cont == E){
+            Console.WriteLine("*");
+        }
+    }
+}
---
+++ ProvaB/Plano de Internet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args){
+        int cota,N,sobrou=0;
+        cota = int.Parse(Console.ReadLine());
+        N = int.Parse(Console.ReadLine());
+        int usados;
+        for(int i = 0; i < N; i++){
+            usados = int.Parse(Console.ReadLine());
+            sobrou += cota;
+            sobrou -= usados;
+        }
+        Console.WriteLine(sobrou + cota);
+    }
+}
