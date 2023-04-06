c166f3b016e5d2c2c96c19f5f2c5b999e0302d82
Raí Átila <raiacavalcante@gmail.com>
2023-04-06 15:26:14 -0300

    Att list Recursividade

--- Recursividade/4- Potencia/Program.cs
+++ Recursividade/4- Potencia/Program.cs
@@ -2,24 +2,23 @@
 class Program{
     public static void Main(string[] args){
         // Declarando variáveis
-        int numero, expoente, resp = 1;
+        int numero, expoente;
         // Entrada de dados
         numero = int.Parse(Console.ReadLine());
         expoente = int.Parse(Console.ReadLine());
         // Chamando a função Recursiva
-        Console.WriteLine(potenciaRecursivo(numero,expoente,resp));
+        Console.WriteLine(potenciaRecursivo(numero,expoente));
     }
 
     // Declarando a função recursiva
-    static int potenciaRecursivo(int numero, int expoente, int resp){
+    static int potenciaRecursivo(int numero, int expoente){
         // Condição de parada 
-        if(expoente == 0){
-            return resp;
+        if(expoente == 1){
+            return numero;
         }
         // Fazendo a operação por Recursividade
         else{
-            resp *= numero;
-            return potenciaRecursivo(numero,expoente-1,resp);
+            return numero * potenciaRecursivo(numero,expoente-1);
         }
     }
 }
