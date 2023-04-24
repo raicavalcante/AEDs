6547338b36afe774cbeb0bd603e94681d875f2d9
Raí Átila <raiacavalcante@gmail.com>
2023-04-23 23:15:14 -0300

    Finaliza TP-2/Q01

---
+++ TP-02/TP2Q01/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args){
+
+        Jogadores[] j = new Jogadores[30];
+        int cont = 0;
+        string entrada = Console.ReadLine();
+        while(entrada != "FIM"){
+            j[cont] = new Jogadores();
+            j[cont].ler(entrada);
+            j[cont].imprimir();
+            cont++;
+            entrada = Console.ReadLine();
+        }
+    }
+}
+class Jogadores{
+    string nome;
+    string foto;
+    string nascimento;
+    int id;
+    int[] times;
+    
+    public void ler(string entrada){
+        int conttimes = 6;
+        string exclui = entrada.Replace('"' , '$');
+        string exclui0 = exclui.Replace("[" , "$");
+        string exclui1 = exclui0.Replace("]" , "$");
+        string exclui2 = exclui1.Replace("$", "");
+        entrada = exclui2;
+        string[] splitada = entrada.Split(',');
+        
+        id = int.Parse(splitada[5]);
+        nome = splitada[1];
+        foto = splitada[2];
+        nascimento = splitada[3];
+        if(splitada.Length <= 7){
+            times = new int[1];
+            times[0] = int.Parse(splitada[6]); 
+        }
+        else{
+            times = new int[splitada.Length-6]; 
+
+            for(int i = 0; i < times.Length; i++){
+                times[i] = int.Parse(splitada[conttimes]);
+                conttimes++;
+            }
+        }
+    }
+    public void imprimir(){
+        Console.Write(id);
+        Console.Write(" ");
+        Console.Write(nome);
+        Console.Write(" ");
+        Console.Write(nascimento);
+        Console.Write(" ");
+        Console.Write(foto);
+        Console.Write(" ");
+        Console.Write("(");
+        for(int i = 0; i < times.Length; i++){
+            if(times.Length == 1){
+                Console.Write(times[i]);
+            }
+            else{
+                Console.Write(times[i]);
+                if(i == times.Length-1){
+                    break;
+                }
+                else{
+                    Console.Write(", ");
+                }
+            }
+        }
+        Console.Write(")");
+        Console.WriteLine();
+    }
+}
