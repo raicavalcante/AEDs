14ffff441499d2ddd37e5e444b6316ca52087a15
Raí Átila <raiacavalcante@gmail.com>
2023-05-14 23:23:36 -0300

    Cria TreinoProva2

---
+++ TreinoProva2/Figurinhas da copa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class Program{
+    public static void Main(string[] args){
+        int qntfigurinhas = int.Parse(Console.ReadLine());
+        int diferentes = 0, repetidas =0;
+        List<int> figurinhas = new List<int>();
+        for (int i = 0; i < qntfigurinhas; i++)
+        {
+            figurinhas.Add(int.Parse(Console.ReadLine()));
+        }
+        figurinhas.Sort();
+        int[] compara = figurinhas.ToArray();
+
+        for (int i = 0; i < qntfigurinhas; i++)
+        {
+            if(compara[i] == compara[i+1]){ // 12 , 65 , 65
+                repetidas++; // 1
+            }
+            else{
+                diferentes++; //1
+            }
+            if(compara[i] == compara[i-1]){
+                
+            }
+        }
+        Console.WriteLine(diferentes);
+        Console.WriteLine(repetidas);
+    }
+}
---
+++ TreinoProva2/Trinca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class Program{
+    public static void Main(string[] args){
+        int qntd = int.Parse(Console.ReadLine());
+        List<int> list = new List<int>();
+        for (int i = 0; i < qntd; i++)
+        {
+            int tam = int.Parse(Console.ReadLine());
+            int[] array = new int[tam];
+            for (int j = 0; j < tam; j++)
+            {
+                array[j] = int.Parse(Console.ReadLine());
+                list.Add(array[j]);
+            }
+            list.Sort();
+            int[] ordenado = list.ToArray();
+            for (int k = 0; k < ordenado.Length; k++)
+            {
+                if(ordenado[k] == ordenado[k+1] && ordenado[k+1] == ordenado[k+2]){
+                    Console.WriteLine(k);
+                }
+                else{
+                    Console.WriteLine(-1);
+                }
+                
+            }
+        }
+    }
+}
